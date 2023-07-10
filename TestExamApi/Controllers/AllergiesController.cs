25379583ff48015999edc539c2661b1bdcc3029c
Sadat Chowdhury <sadatjnu9@gmail.com>
2023-07-11 02:20:19 +0600

    update

---
+++ TestExamApi/Controllers/AllergiesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestExamApi.Data;
+using TestExamApi.Entites;
+
+namespace TestExamApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AllergiesController : BaseController<Allergies>
+    {
+        public AllergiesController(IRepository<Allergies> repository) : base(repository)
+        {
+            
+        }
+    }
+}
---
+++ TestExamApi/Controllers/NCDController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestExamApi.Data;
+using TestExamApi.Entites;
+
+namespace TestExamApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NCDController : BaseController<NCD>
+    {
+        public NCDController(IRepository<NCD> repository) : base(repository)
+        {
+            
+        }
+    }
+}
